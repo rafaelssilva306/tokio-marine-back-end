749bf9ba35bca1afc8782e7873e7457cc3b15dda
Rafael Silva <rafa.306@hotmail.com>
2023-06-23 16:33:43 -0300

    criado o service e adicionada as configurações do H2

---
+++ src/main/java/com/tokiobank/service/TransacaoFinanceiraService.java
@@ -0,0 +1,27 @@
+package com.tokiobank.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tokiobank.model.TransacaoFinanceira;
+import com.tokiobank.repositories.TransacaoFinanceiraRepository;
+
+@Service
+public class TransacaoFinanceiraService {
+	
+	@Autowired
+	private TransacaoFinanceiraRepository repository;
+	
+	public TransacaoFinanceira cadastrar(TransacaoFinanceira transacao) {
+		
+		repository.save(transacao);
+		return transacao;
+	}
+	
+	public List<TransacaoFinanceira> consultar(){
+		return repository.findAll();
+	}
+
+}
