018625729dfb49e34adf54e5eb52541d7ec262bf
Rafael Silva <rafa.306@hotmail.com>
2023-06-23 16:18:57 -0300

    Commit inicial, TESTE TOKIO MARINE

---
+++ src/main/java/com/tokiobank/TokioBankApplication.java
@@ -0,0 +1,13 @@
+package com.tokiobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokioBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokioBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tokiobank/TokioBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokioBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
