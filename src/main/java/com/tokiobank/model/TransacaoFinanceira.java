32ee59147966cb8de84739cc84db5d1c7990f92f
Rafael Silva <rafa.306@hotmail.com>
2023-06-23 16:21:56 -0300

    Criada a primeira entidade TransacaoFinanceira

---
+++ src/main/java/com/tokiobank/model/TransacaoFinanceira.java
@@ -0,0 +1,85 @@
+package com.tokiobank.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TransacaoFinanceira {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contaOrigem;
+
+    private String contaDestino;
+
+    private Double taxa;
+
+    private LocalDate dataAgendamento;
+
+    private LocalDate dataTransferencia;
+
+    private Double valorTransferencia;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(String contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public String getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(String contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public Double getValorTransferencia() {
+        return valorTransferencia;
+    }
+
+    public void setValorTransferencia(Double valorTransferencia) {
+        this.valorTransferencia = valorTransferencia;
+    }
+
+    public Double getTaxa() {
+        return taxa;
+    }
+
+    public void setTaxa(Double taxa) {
+        this.taxa = taxa;
+    }
+
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDate dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+
+    public LocalDate getDataTransferencia() {
+        return dataTransferencia;
+    }
+
+    public void setDataTransferencia(LocalDate dataTransferencia) {
+        this.dataTransferencia = dataTransferencia;
+    }
+    
+}
